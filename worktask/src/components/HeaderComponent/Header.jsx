5f71833aa73b907989dff2c8ff4f330afc5703da
Gianni <gianni.mondello@hotmail.fr>
2023-04-27 16:14:48 +0200

    ajout font et header

--- worktask/src/components/HeaderComponent/Header.jsx
+++ worktask/src/components/HeaderComponent/Header.jsx
@@ -3,9 +3,16 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+
+        <div className="header">
+            <div id='top_header'>
+                <h2 className='title_header'>WorkTask</h2>
+            </div>
+            <div id='bot_header'>
+                <p className='sous_titre'>Facilitez vous la task</p>
+            </div>
         </div>
+
     );
 };
 
