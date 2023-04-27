dea3966b576c344167f391cd0fb9d1e888b3d5e0
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-04-27 16:48:01 +0200

    MAJ avec BeforeMain

--- worktask/src/App.js
+++ worktask/src/App.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/HeaderComponent/Header';
 
 
@@ -15,4 +13,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 662f132695d350e97dc0bc83662f65da7db6d077
