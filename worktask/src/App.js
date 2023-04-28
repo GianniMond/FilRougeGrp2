4881d97f55ef6c3aa93b56280c1d65ace94be032
Pamela Chombeau <pamela.chombeau@outlook.fr>
2023-04-28 09:53:58 +0200

    ajout du logo au header

--- worktask/src/App.js
+++ worktask/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/HeaderComponent/Header';
 import Footer from './components/FooterComponent/Footer';
--- worktask/src/components/HeaderComponent/Header.jsx
+++ worktask/src/components/HeaderComponent/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import image from "../../logo_prev_ui.png";
 
 const Header = () => {
     return (
 
         <div className="header">
+            <img className='logo' src={image} alt="logo du site" height= '95px'/>
             <div id='top_header'>
                 <h2 className='title_header'>WorkTask</h2>
-            </div>
-            <div id='bot_header'>
                 <p className='sous_titre'>Facilitez vous la task</p>
             </div>
+            {/* <div id='bot_header'>
+                
+            </div> */}
         </div>
 
     );
