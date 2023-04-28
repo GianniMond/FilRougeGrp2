06c35d795dcca0524afed87304a695ad5fc37fdb
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-04-28 14:19:44 +0200

    conflit

--- worktask/src/views/DetailProjectListView/DetailProjectList.jsx
+++ worktask/src/views/DetailProjectListView/DetailProjectList.jsx
@@ -4,8 +4,18 @@ import './DetailProjectList.css';
 
 const DetailProjectList = () => {
     return (
-        <div>
-            
+        <div className='background'>
+            <div className='menu-left'>
+                <div>membres du projet</div>
+                <div>Ajouter tache</div>
+            </div>
+            <div>
+                <div>Importante</div>
+                <div>A faire</div>
+                <div>En cours</div>
+                <div>A vérifier</div>
+                <div>Terminée</div>
+            </div>
         </div>
     );
 };
