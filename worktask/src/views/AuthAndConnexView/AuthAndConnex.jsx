1c0813b636e8bb31a00898c4eec6539bd329f7c5
Gianni <gianni.mondello@hotmail.fr>
2023-04-28 14:35:02 +0200

    mise a jour totale

--- worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
+++ worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
@@ -16,15 +16,9 @@ const AuthAndConnex = () => {
         navigate("/projetList");
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     return (
-=======
-    return (     
->>>>>>> LucieD
-=======
-    return (
->>>>>>> db121e241546c52256aa951060da4d9cbc7633a7
+
         <div className='background'>
             <form class="form-group">
                 <div class="demandes">
@@ -33,17 +27,10 @@ const AuthAndConnex = () => {
                     <label for="motDePasseConnex">Mot de passe</label>
                     <input type="password" className="form-control" id="motDePasseConnex" placeholder="******" />
                 </div>
-<<<<<<< HEAD
                 <div class="boutons">
                     <button type="submit" onClick={handleConnexionClick} className="btn btn_connexview">Connexion</button>
                     <button type="submit" onClick={handleInscriptionClick} className="btn btn_connexview">Inscription</button>
                 </div>
-=======
-                <div>
-                    <button type="submit" onClick={handleConnexionClick} className="btn btn-secondary btn_connexview">Connexion</button>
-                </div>
-                <button type="submit" onClick={handleInscriptionClick} className="btn btn-secondary btn_connexview">Inscription</button>
->>>>>>> db121e241546c52256aa951060da4d9cbc7633a7
             </form>
         </div>
     );
