bbebfccd96276aed5eb09623888171f66caea7f4
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-04-28 12:31:55 +0200

    finition page connexion + MAJ normesCss

--- worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
+++ worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
@@ -4,20 +4,18 @@ import './AuthAndConnex.css';
 
 const AuthAndConnex = () => {
     return (
-        <div>
-            <form>
-                <div class="form-group">
-                    <label for="emailConnex">Email</label>
-                    <input type="email" className="form-control" id="emailConnex" placeholder="Entrez votre adresse mail" />
-                </div>
-                <div class="form-group">
+        <div className='background'>
+            <form class="form-group">
+                <div class="demandes">
+                    <label for="emailConnex">Adresse mail</label>
+                    <input type="email" className="form-control" id="emailConnex" placeholder="abcdefghij@gmail.com" />
                     <label for="motDePasseConnex">Mot de passe</label>
-                    <input type="password" className="form-control" id="motDePasseConnex" placeholder="Mot de passe" />
+                    <input type="password" className="form-control" id="motDePasseConnex" placeholder="******" />
                 </div>
-                <div>
-                    <button type="submit" className="btn btn-secondary btn_connexview">Connexion</button>
+                <div class="boutons">
+                    <button type="submit" className="btn btn_connexview">Connexion</button>
+                    <button type="submit" className="btn btn_connexview">Inscription</button>
                 </div>
-                <button type="submit" className="btn btn-secondary btn_connexview">Inscription</button>
             </form>
 
         </div>
