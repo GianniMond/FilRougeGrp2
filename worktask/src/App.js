98515550bab00cfe18735a8b08499a5ce5052ccc
Pamela Chombeau <pamela.chombeau@outlook.fr>
2023-04-28 11:29:20 +0200

    ...

--- worktask/src/App.js
+++ worktask/src/App.js
@@ -3,14 +3,18 @@ import Header from './components/HeaderComponent/Header';
 import Footer from './components/FooterComponent/Footer';
 
 
+
+
 function App() {
   return (
-
+   
     <div className="App">
       <Header />
       <Footer />
     </div>
+
   );
 }
 
 export default App;
+
