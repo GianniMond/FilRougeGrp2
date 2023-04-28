e47f6b4b672735d9c62324117859a36d7ea6e44f
Gianni <gianni.mondello@hotmail.fr>
2023-04-28 14:10:03 +0200

    merge ok

--- worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
+++ worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
@@ -17,22 +17,6 @@ const AuthAndConnex = () => {
     }
 
     return (
-<<<<<<< HEAD
-        <div>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="emailConnex">Email</label>
-                    <input type="email" className="form-control" id="emailConnex" placeholder="Entrez votre adresse mail" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="motDePasseConnex">Mot de passe</label>
-                    <input type="password" className="form-control" id="motDePasseConnex" placeholder="Mot de passe" />
-                </div>
-                <div>
-                    <button type="submit" onClick={handleConnexionClick} className="btn btn-secondary btn_connexview">Connexion</button>
-                </div>
-                <button type="submit" onClick={handleInscriptionClick} className="btn btn-secondary btn_connexview">Inscription</button>
-=======
         <div className='background'>
             <form class="form-group">
                 <div class="demandes">
@@ -41,11 +25,10 @@ const AuthAndConnex = () => {
                     <label for="motDePasseConnex">Mot de passe</label>
                     <input type="password" className="form-control" id="motDePasseConnex" placeholder="******" />
                 </div>
-                <div class="boutons">
-                    <button type="submit" className="btn btn_connexview">Connexion</button>
-                    <button type="submit" className="btn btn_connexview">Inscription</button>
+                <div>
+                    <button type="submit" onClick={handleConnexionClick} className="btn btn-secondary btn_connexview">Connexion</button>
                 </div>
->>>>>>> LucieD
+                <button type="submit" onClick={handleInscriptionClick} className="btn btn-secondary btn_connexview">Inscription</button>
             </form>
         </div>
     );
