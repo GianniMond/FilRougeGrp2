77a0135fde8272dc140dda13a12f93d19ee8cb07
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-04-28 14:24:01 +0200

    maj

--- worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
+++ worktask/src/views/AuthAndConnexView/AuthAndConnex.jsx
@@ -18,22 +18,6 @@ const AuthAndConnex = () => {
     }
 
     return (
-<<<<<<< HEAD
-        <div>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="emailConnex">Email</label>
-                    <input type="email" className="form-control" id="emailConnex" placeholder="Entrez votre adresse mail" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="motDePasseConnex">Mot de passe</label>
-                    <input type="password" className="form-control" id="motDePasseConnex" placeholder="Mot de passe" />
-                </div>
-                <div>
-                    <button type="submit" onClick={handleConnexionClick} className="btn btn-secondary btn_connexview">Connexion</button>
-                </div>
-                <button type="submit" onClick={handleInscriptionClick} className="btn btn-secondary btn_connexview">Inscription</button>
-=======
         <div className='background'>
             <form class="form-group">
                 <div class="demandes">
@@ -46,7 +30,6 @@ const AuthAndConnex = () => {
                     <button type="submit" className="btn btn_connexview">Connexion</button>
                     <button type="submit" className="btn btn_connexview">Inscription</button>
                 </div>
->>>>>>> LucieD
             </form>
         </div>
     );
