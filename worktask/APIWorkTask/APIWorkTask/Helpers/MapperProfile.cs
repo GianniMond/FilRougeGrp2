0e3cc3ee75e6e581e3fc63e9e212a2d139737e50
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-05-31 12:15:06 +0200

    update API avec DTOs, Helper et DTOs controllers

---
+++ worktask/APIWorkTask/APIWorkTask/Helpers/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using APIWorkTask.Models;
+using APIWorkTask.DTOs;
+using AutoMapper;
+
+namespace APIWorkTask.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Personne, PersonneDTO>().ReverseMap(); //cette ligne permet de dire qu'a l'aide du mapper on pourra passer de l'entité vers le DTO
+        }
+    }
+}
