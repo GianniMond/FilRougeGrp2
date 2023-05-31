3f87dbcf45776077396533ba9199b896293917f2
Lucie Duriez <lucieduriez.etu@gmail.com>
2023-05-31 12:21:06 +0200

    modif program.cs dans API

--- worktask/APIWorkTask/APIWorkTask/Program.cs
+++ worktask/APIWorkTask/APIWorkTask/Program.cs
@@ -1,4 +1,5 @@
 using APIWorkTask.Data;
+using APIWorkTask.Models;
 using APIWorkTask.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel;
@@ -6,10 +7,14 @@ using Microsoft.IdentityModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 //void ConfigureServices(IServiceCollection services)
 //{
 //    services.AddDbContext<ApplicationDbContext>(options =>
